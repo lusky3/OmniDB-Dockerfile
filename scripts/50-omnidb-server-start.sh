306b4d8958098dbf653ac575cf98a44da0144497
Cody <ChaosZero112@users.noreply.github.com>
2020-11-08 05:06:10 -0500

    Update and rename 40-omnidb-server.sh to 50-omnidb-server-start.sh

--- scripts/50-omnidb-server-start.sh
+++ scripts/50-omnidb-server-start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [[ ! /omnidb-server/config.py ]]; then
-  python3 /opt/omnidb/OmniDB/omnidb-server.py --homedir=/omnidb-server --init
-fi
 python3 /opt/omnidb/OmniDB/omnidb-server.py --homedir=/omnidb-server
 echo -e "If you are reading this then something went wrong."
 exit 1
