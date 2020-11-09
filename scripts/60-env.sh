d528cf77a1ce36f7706c31820ecf1db1865b4de2
Cody <ChaosZero112@users.noreply.github.com>
2020-11-09 17:33:00 -0500

    Update 60-env.sh

--- scripts/60-env.sh
+++ scripts/60-env.sh
@@ -7,15 +7,15 @@ else
   echo -e "Modifying config.py with ENV values..."
   echo -e "(Tip: Add empty file config.lock to /omnidb-server to prevent overwriting settings)"
   cd /omnidb-server
-  if [[ $^LISTENING_PORT != "8000" ]]; then
-    sed -i "s/LISTENING_PORT[[:blank:]]*=.*$/LISTENING_PORT       = ${LISTENING_ADDRESS}/g" config.py
+  if [[ ! -z $LISTENING_PORT ]]; then
+    sed -i "s/^LISTENING_PORT[[:blank:]]*=.*$/LISTENING_PORT       = ${LISTENING_ADDRESS}/g" config.py
   fi
-  if [[ $LISTENING_ADDRESS != "127.0.0.1" ]]; then
+  if [[ ! -z $LISTENING_ADDRESS ]]; then
     sed -i "s/^LISTENING_ADDRESS[[:blank:]]*=  .*$/LISTENING_ADDRESS    = '${LISTENING_ADDRESS}'/g" config.py
   fi
   if [[ $IS_SSL == "true" ]] || [[ $IS_SSL == "True" ]] || [[ $IS_SSL == "Yes" ]] || [[ $IS_SSL == "yes" ]]; then
     sed -i "s/^IS_SSL[[:blank:]]*=.*$/IS_SSL                 = True/g" config.py
-  elif [[ $IS_SSL == "false" ]] || [[ $IS_SSL == "False" ]] || [[ $IS_SSL == "No" ]] || [[ $IS_SSL == "no" ]]; then
+  else
     sed -i "s/^IS_SSL[[:blank:]]*=.*$/IS_SSL                 = False/g" config.py
   fi
   if [[ ! -z $SSL_CERTIFICATE_FILE ]]; then
@@ -24,13 +24,17 @@ else
   if [[ ! -z $SSL_KEY_FILE ]]; then
     sed -i "s/^SSL_KEY_FILE[[:blank:]]*= .*$/SSL_KEY_FILE           = '${SSL_KEY_FILE}'/g" config.py
   fi
-  if [[ ! -z $SESSION_COOKIE_SECURE ]]; then
-    sed -i "s/^#SESSION_COOKIE_SECURE[[:blank:]]*= .*$/SESSION_COOKIE_SECURE  = '${SESSION_COOKIE_SECURE}'/g" config.py
-    sed -i "s/^SESSION_COOKIE_SECURE[[:blank:]]*= .*$/SESSION_COOKIE_SECURE  = '${SESSION_COOKIE_SECURE}'/g" config.py
+  if [[ $SESSION_COOKIE_SECURE == "true" ]] || [[ $SESSION_COOKIE_SECURE == "True" ]] || [[ $SESSION_COOKIE_SECURE == "Yes" ]] || [[ $SESSION_COOKIE_SECURE == "yes" ]]; then
+    sed -i "s/^#SESSION_COOKIE_SECURE[[:blank:]]*=.*$/SESSION_COOKIE_SECURE  = True/g" config.py
+    sed -i "s/^SESSION_COOKIE_SECURE[[:blank:]]*=.*$/SESSION_COOKIE_SECURE  = True/g" config.py
+  else
+    sed -i "s/^SESSION_COOKIE_SECURE[[:blank:]]*=.*$/SESSION_COOKIE_SECURE  = False/g" config.py
   fi
-  if [[ ! -z $CSRF_COOKIE_SECURE ]]; then
-    sed -i "s/^#CSRF_COOKIE_SECURE[[:blank:]]*= .*$/CSRF_COOKIE_SECURE     = '${CSRF_COOKIE_SECURE}'/g" config.py
-    sed -i "s/^CSRF_COOKIE_SECURE[[:blank:]]*= .*$/CSRF_COOKIE_SECURE     = '${CSRF_COOKIE_SECURE}'/g" config.py
+  if [[ $CSRF_COOKIE_SECURE == "true" ]] || [[ $CSRF_COOKIE_SECURE == "True" ]] || [[ $CSRF_COOKIE_SECURE == "Yes" ]] || [[ $CSRF_COOKIE_SECURE == "yes" ]]; then
+    sed -i "s/^#CSRF_COOKIE_SECURE[[:blank:]]*=.*$/CSRF_COOKIE_SECURE     = True/g" config.py
+    sed -i "s/^CSRF_COOKIE_SECURE[[:blank:]]*=.*$/CSRF_COOKIE_SECURE     = True/g" config.py
+  else
+    sed -i "s/^CSRF_COOKIE_SECURE[[:blank:]]*=.*$/CSRF_COOKIE_SECURE     = False/g" config.py
   fi
   if [[ ! -z $CUSTOM_PATH ]]; then
     sed -i "s/^CUSTOM_PATH[[:blank:]]*= .*$/CUSTOM_PATH    = '${CUSTOM_PATH}'/g" config.py
