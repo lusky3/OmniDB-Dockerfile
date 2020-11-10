a28e42c19822713b0f18013d3b1fe05d6616622b
Cody <ChaosZero112@users.noreply.github.com>
2020-11-10 00:37:37 -0500

    Update and rename 50-omnidb-init.sh to 30-omnidb-init.sh

--- scripts/30-omnidb-init.sh
+++ scripts/30-omnidb-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ ! -f /omnidb-server/config.py ]]; then
   echo -e "Initializing OmniDB home folder..."
-  python3 /opt/omnidb/OmniDB/omnidb-server.py --homedir=/omnidb-server --init
+  /opt/omnidb-server/omnidb-server --homedir=/omnidb-server --init
 else
   echo -e "config.py was found at /omnidb-server. Skipping initialization."
   echo -e "(If you want to force initialization, please remove config.py)"
