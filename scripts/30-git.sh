c9235d64f1e344bd0e6188e6de33040cba790547
Cody <ChaosZero112@users.noreply.github.com>
2020-11-10 00:35:47 -0500

    Delete 30-git.sh

--- scripts/30-git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -e "Updating OmniDB repository..."
-cd /opt/omnidb && \
-git fetch origin master --quiet && \
-git reset --hard origin/master --quiet && \
-git clean -fd --quiet &&
-git pull --quiet
-exit 0
