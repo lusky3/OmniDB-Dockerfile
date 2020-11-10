649c5349ef745265840356539519d0da133f4219
Cody <ChaosZero112@users.noreply.github.com>
2020-11-10 00:39:02 -0500

    Update and rename 70-omnidb-server-start.sh to 50-omnidb-server-start.sh

--- scripts/50-omnidb-server-start.sh
+++ scripts/50-omnidb-server-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 /opt/omnidb/OmniDB/omnidb-server.py --homedir=/omnidb-server
+/opt/omnidb-server/omnidb-server --homedir=/omnidb-server
 echo -e "If you are reading this then something went wrong."
 exit 1
